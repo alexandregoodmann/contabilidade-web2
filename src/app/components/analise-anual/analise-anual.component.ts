5adac9c0a7770ac678f2dde3e266ae277d160866
alexandregoodmann <alexandregoodmann@gmail.com>
2022-11-06 17:26:49 -0300

    Fix analise anual

--- src/app/components/analise-anual/analise-anual.component.ts
+++ src/app/components/analise-anual/analise-anual.component.ts
@@ -50,7 +50,7 @@ export class AnaliseAnualComponent implements OnInit {
       const entradas = lancamentosMes.filter(o => o.valor > 0);
       const entrada = entradas.map(n => n.valor).reduce((a, b) => a + b);
       const saidas = lancamentosMes.filter(o => o.valor < 0 && o.categoria != 'Cartão' && o.concluido);
-      const saida = saidas.map(n => n.valor).reduce((a, b) => a + b);
+      let saida = (saidas.length > 0) ? saidas.map(n => n.valor).reduce((a, b) => a + b) : 0;
       linha[1] = entrada + saida;
       matriz.push(linha);
     });
