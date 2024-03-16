cb202ea62cee7991d42855fcc266a872865e90f0
alexandregoodmann <alexandregoodmann@gmail.com>
2024-03-15 22:05:44 -0300

    saldo diario

--- src/app/components/saldo-diario/saldo-diario.component.ts
+++ src/app/components/saldo-diario/saldo-diario.component.ts
@@ -10,45 +10,53 @@ import { ExtratoService } from 'src/app/services/extrato.service';
 })
 export class SaldoDiarioComponent implements OnInit {
 
-  graphicDataSource!: Lancamento[];
-  ds: any;
-  constructor(private extratoService: ExtratoService) { }
+  extrato!: Lancamento[];
+  dataSource: any;
+
+  constructor(
+    private extratoService: ExtratoService,
+  ) { }
 
   ngOnInit(): void {
-    this.graphicDataSource = this.extratoService.datasource;
-    this.processMapa();
+    this.extratoService.extrato.subscribe(data => {
+      this.extrato = data;
+      console.log(this.extrato);
+      this.processDataSource();
+    });
   }
 
-  processMapa() {
+  processDataSource() {
+
+    
+    // convert to date
+    this.extrato.forEach(i => { i.data = new Date(i.data) });
+
+    // get last day of the month
+    let mes = new Date();
+    let lastDayOfMonth = new Date(mes.getFullYear(), mes.getMonth() + 1, 0).getDate();
+
     let mapa = new Map<number, number>();
-    this.graphicDataSource.forEach(data => {
-      let d = new Date(data.data);
-      let day = d.getDate();
-      if (mapa.has(day)) {
-        let valor = (mapa.get(day) as number) + data.valor;
-        mapa.set(day, valor);
-      } else {
-        mapa.set(day, data.valor);
-      }
-    });
+    for (let index = 1; index <= lastDayOfMonth; index++) {
+      let lancamentosDia = this.extrato.filter(o => (o.data as Date).getDate() == index && o.conta.tipo == 'CC');
+      let saldoDia = (lancamentosDia.length == 0) ? 0 : lancamentosDia.map(o => o.valor).reduce((a, b) => a + b);
+      saldoDia = (index == 1) ? saldoDia : (saldoDia + (mapa.get(index - 1) as number));
+      mapa.set(index, saldoDia);
+    }
 
-    this.ds = [];
+    this.dataSource = [];
     mapa.forEach((v, k) => {
-      this.ds.push([k, v]);
+      let color = (v > 0) ? '#3366cc' : '#dc3912';
+      this.dataSource.push([k, v, color]);
     });
-
-    this.graficoBarra.datasource = this.ds
-
-    console.log(this.ds);
+    this.graficoBarra.datasource = this.dataSource
 
   }
-
 
   graficoBarra = {
     title: 'Saldo Diário',
-    columnNames: ['Dia', 'saldo'],
+    columnNames: ['Dia', 'saldo', { role: 'style' }],
     type: ChartType.ColumnChart,
-    datasource: [[1, 1000], [2, 1500]],
+    datasource: [[1, 1000, '#3366cc']],
     options: {
       legend: { position: "none" }
     }
